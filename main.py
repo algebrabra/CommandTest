aac77218087885f404f21a4a00ea2e70d5d935f8
algebrabra <83385848+algebrabra@users.noreply.github.com>
2022-10-19 17:53:22 +0800

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -2,10 +2,41 @@ def on_button_pressed_a():
     global i
     i += 1
     basic.show_number(i)
+    music.play_sound_effect(music.create_sound_effect(WaveShape.SINE,
+            0,
+            3000,
+            255,
+            0,
+            500,
+            SoundExpressionEffect.NONE,
+            InterpolationCurve.LINEAR),
+        SoundExpressionPlayMode.IN_BACKGROUND)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def on_button_pressed_b():
+    global i
+    i += -1
+    basic.show_number(i)
+    music.play_sound_effect(music.create_sound_effect(WaveShape.SINE,
+            3000,
+            0,
+            255,
+            0,
+            500,
+            SoundExpressionEffect.NONE,
+            InterpolationCurve.LINEAR),
+        SoundExpressionPlayMode.IN_BACKGROUND)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def resetTimer():
+    global timer
+    timer = 1000
+timer = 0
 i = 0
+state = 0
+resetTimer()
 i = 0
+basic.show_number(i)
 
 def on_forever():
     pass
--- main.ts
+++ main.ts
@@ -1,12 +1,20 @@
 input.onButtonPressed(Button.A, function () {
     i += 1
     basic.showNumber(i)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 0, 3000, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
 })
 input.onButtonPressed(Button.B, function () {
     i += -1
     basic.showNumber(i)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 3000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
 })
+function resetTimer () {
+    timer = 1000
+}
+let timer = 0
 let i = 0
+let state = 0
+resetTimer()
 i = 0
 basic.showNumber(i)
 basic.forever(function () {
