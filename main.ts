61af5d0c6f6e05ded9f20943282ae8d6b4640567
algebrabra <83385848+algebrabra@users.noreply.github.com>
2022-10-22 12:25:47 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,11 +2,13 @@ input.onButtonPressed(Button.A, function () {
     i += 1
     basic.showNumber(i)
     music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 0, 3000, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
+    resetTimer()
 })
 input.onButtonPressed(Button.B, function () {
     i += -1
     basic.showNumber(i)
     music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 3000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
+    resetTimer()
 })
 function resetTimer () {
     timer = 1000
@@ -18,5 +20,10 @@ resetTimer()
 i = 0
 basic.showNumber(i)
 basic.forever(function () {
-	
+    timer += -100
+    basic.pause(100)
+    if (timer == 0) {
+        basic.showIcon(IconNames.TShirt)
+        resetTimer()
+    }
 })
